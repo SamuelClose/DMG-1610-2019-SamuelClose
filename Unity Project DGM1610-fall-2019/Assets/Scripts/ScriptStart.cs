dfe056144a204463bc774b919e56e6839311cce3
Samuel Close <10911389@my.uvu.edu>
2019-08-29 15:49:32 -0600

    Unity project changes

---
+++ Unity Project DGM1610-fall-2019/Assets/Scripts/ScriptStart.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptStart : MonoBehaviour {
+
+	// Use this for initialization or start
+	private void Start() 
+	{
+		print("Hello World"); 
+		print("I love you");
+		print("Nicu");
+	}
+}
