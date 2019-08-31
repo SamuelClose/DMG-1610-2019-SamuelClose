86e5916021085eda91116d21c007aa37af8ddf86
Samuel Close <10911389@my.uvu.edu>
2019-08-30 21:29:25 -0600

    Scripts practice.... I have no idea what Im doing research more tomorrow

---
+++ Unity Project DGM1610-fall-2019/Assets/Scripts/ScriptPractice.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptPractice : MonoBehaviour
+{
+
+	int Myint = (5);
+
+	// Use this for initialization
+	void Start()
+	{
+		
+	}
+
+	//intMultiplyByTwo
+	{
+		int;
+		int =  return *2
+		return; 
+	}
+}
+
+// Update is called once per frame
+	void Update () {
+		
+	}
+}
+// I tried but I hsve no idea what I am doing... research a tutuorial on better ways to read syntax and conventions for C#
