b976b1c74e808e8fb0266cb0d048dc6399577f1e
Samuel Close <10911389@my.uvu.edu>
2019-09-03 15:25:53 -0600

    Unity practice using Variables

---
+++ Unity Project DGM1610-fall-2019/Assets/Scripts/Variables.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+public class Variables : MonoBehaviour
+{
+	//Float numbers require a f after the numeral
+	//int do not need f after it 
+	public float floatValue = 5.5f;
+	public int intValue = 20;
+	public string stringValue = "Bob";
+	public int firepower;
+	public UnityEvent 
+
+
+
+	private void OnTriggerEnter (Collider other) 
+	{
+		print("bob");
+	}
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	private void Update () {
+		
+	}
+}
+
