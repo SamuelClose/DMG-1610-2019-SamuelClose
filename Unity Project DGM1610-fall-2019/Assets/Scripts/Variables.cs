3af8d6237e2f8aeb4388775e69446997ca972c3e
Samuel Close <10911389@my.uvu.edu>
2019-09-06 23:13:21 -0600

    Events

--- Unity Project DGM1610-fall-2019/Assets/Scripts/Variables.cs
+++ Unity Project DGM1610-fall-2019/Assets/Scripts/Variables.cs
@@ -11,16 +11,37 @@ public class Variables : MonoBehaviour
 	//Float numbers require a f after the numeral
 	//int do not need f after it 
 	public float floatValue = 5.5f;
-	public int intValue = 20;
+	private int intValue = 20;
 	public string stringValue = "Bob";
 	public int firepower;
 	public UnityEvent Event;
 
+	private void Start()
+	{
+		{
+			intValue = 33;
+			Debug.Log(intValue * 2);
+		}
+		
+		{
+			/*
+			Debug.Log(transform.position.x);
+			
+			if (transform.position.y <= 5f)
+			{
+				Debug.Log("BANZAI");
+			}
+			*/
+		}
+		
+	}
+
 
 	private void OnTriggerEnter(Collider other)
 	{
-		if (!Input.GetKeyDown(KeyCode.DownArrow)) return;
-		Destroy(gameObject);
-		DontDestroyOnLoad(gameObject);
+		Event.Invoke();
+	
 	}
+	
+	
 }
