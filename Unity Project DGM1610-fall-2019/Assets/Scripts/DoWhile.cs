6ab6a4d373a4ad45a77422e846de71d375d65e01
Samuel Close <10911389@my.uvu.edu>
2019-09-07 00:05:21 -0600

    Loops
    While, DoWhile, and ForWhile

---
+++ Unity Project DGM1610-fall-2019/Assets/Scripts/DoWhile.cs
@@ -0,0 +1,19 @@
+﻿
+using UnityEngine;
+
+public class DoWhile : MonoBehaviour
+{
+	private void Start()
+	{
+		const bool shouldContinue = true;
+
+		do
+		{
+			print("FanX was amaze balls");
+		} 
+		while (shouldContinue == false);
+	}
+}
+
+
+	
---
+++ Unity Project DGM1610-fall-2019/Assets/Scripts/ForLoop.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class ForLoop : MonoBehaviour
+{
+	private int numEnemies = 4;
+
+	private void Start()
+	{
+		for (var i = 0; i < numEnemies; i++)
+		{
+			Debug.Log("ENEMIES" + i);
+		}
+	}
+}
+
+	
