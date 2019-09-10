28670a4479bd18e2fb39880cfc6920ab35c9770d
Samuel Close <10911389@my.uvu.edu>
2019-09-09 21:21:14 -0600

    working on events

---
+++ Unity Project DGM1610-fall-2019/Assets/Scripts/Class01.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+public class Class01 : MonoBehaviour
+{
+
+
+	public void OnTriggerEnter(Collider other)
+	{
+		if () ;
+	}
+
+}
+
+
+// Public allows for editing outside the script as well as accessed by other script
+//Private means has to be changed within script
+//awake is used before the start function, for example if an enemy has spawned the awake can state the amount of bullets he has, and then he wont shoot till the start function.
+// Fixed update will have the same time between each update
+
