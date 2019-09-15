8cec3d1064524dfffa4d090482bde6d97d6c4793
Samuel Close <10911389@my.uvu.edu>
2019-09-14 18:53:28 -0600

    Events

---
+++ DGM1610 Restart/Assets/Scripts/MovementUp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MovementUp : MonoBehaviour
+{
+   public UnityEvent jump;
+
+
+   private void OnMouseDown()
+   {
+      jump.Invoke();
+   }
+}
