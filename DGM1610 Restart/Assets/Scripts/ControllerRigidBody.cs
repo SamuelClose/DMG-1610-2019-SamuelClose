d9ec70d6ca91475afd9e0a69e45ea331a4213ed2
Samuel Close <10911389@my.uvu.edu>
2019-09-25 04:50:24 -0600

    Character jump

--- DGM1610 Restart/Assets/Scripts/ControllerRigidBody.cs
+++ DGM1610 Restart/Assets/Scripts/ControllerRigidBody.cs
@@ -4,29 +4,78 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Serialization;
 
+[RequireComponent(typeof(Rigidbody))]
+
 public class ControllerRigidBody : MonoBehaviour
 {
+
+    private Vector3 pos;
+    
+    public int jumpCount;
+
+    public int jumpCountMax = 2;
+    
+    public LayerMask groundLayers;
+
+    public CapsuleCollider capsule;
+    
     private string moveInputAxis = "Vertical";
+    
     private string turnInputAxis = "Horizontal";
 
-    public float rotationRate = 360;
+    public float rotationRate = 360f;
+    
     public float moveSpeed = 10f;
 
-    private Rigidbody rb;
+    public float jumpSpeed = 10f;
 
+    public float gravity = 9.81f;
+    
+    
+
+    private Rigidbody rb;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+        capsule = GetComponent<CapsuleCollider>();
     }
 
-    void Update()
+    void FixedUpdate()
+    
     {
+        
+        pos.y -= gravity;
+        
         float moveAxis = Input.GetAxis(moveInputAxis);
         float turnAxis = Input.GetAxis(turnInputAxis);
         
+
         ApplyInput(moveAxis, turnAxis);
+        
+        
+        if (IsGrounded() && Input.GetKeyDown(KeyCode.Space) && jumpCount < jumpCountMax)
+        {
+            rb.AddForce(Vector3.up * jumpSpeed, ForceMode.Impulse);
+     
+            jumpCount++;
+        }
+
+        
     }
+
+    private bool IsGrounded()
+    {
+        jumpCount = 0;
+        
+        return Physics.CheckCapsule(capsule.bounds.center,
+            new Vector3(capsule.bounds.center.x, capsule.bounds.min.y, capsule.bounds.center.z), capsule.radius * .9f, groundLayers);
+        
+    }
+
+
+
+
 
     private void ApplyInput(float moveInput, float turnInput)
     {
@@ -37,15 +86,17 @@ public class ControllerRigidBody : MonoBehaviour
 
     private void Move(float input)
     {
-       //transform.Translate(moveSpeed * input * Vector3.forward);
-       rb.AddForce(moveSpeed * input * transform.forward, ForceMode.Force);
+        rb.AddForce(moveSpeed * input * transform.forward, ForceMode.Force);
     }
 
     private void Turn(float input)
     {
         transform.Rotate(0, input * rotationRate * Time.deltaTime, 0);
     }
-
-
+    
 
 }
+
+
+
+
