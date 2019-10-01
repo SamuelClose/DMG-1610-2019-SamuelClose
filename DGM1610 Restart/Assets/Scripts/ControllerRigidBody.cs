917228d9145c3ebcb3ce0df4f8033f4137a2ed0c
Samuel Close <10911389@my.uvu.edu>
2019-10-01 16:01:30 -0600

    IN class Scriptiable Objects

--- DGM1610 Restart/Assets/Scripts/ControllerRigidBody.cs
+++ DGM1610 Restart/Assets/Scripts/ControllerRigidBody.cs
@@ -31,30 +31,17 @@ public class ControllerRigidBody : MonoBehaviour
 
     private Rigidbody rb;
     
-
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
         cap = GetComponent<CapsuleCollider>();
     }
-
-
-   
-
     void FixedUpdate()
-    
     {
         float moveAxis = Input.GetAxis(moveInputAxis);
         float turnAxis = Input.GetAxis(turnInputAxis);
-        
-
         ApplyInput(moveAxis, turnAxis);
-        
-      
-       
         rb.AddForce(Vector3.down * gravity, ForceMode.Acceleration);
-        
-        
         
         if (IsGrounded() && Input.GetKeyDown(KeyCode.Space) && jumpCount < jumpCountMax)
         {
@@ -62,28 +49,22 @@ public class ControllerRigidBody : MonoBehaviour
             jumpCount++;
         }
     }
-
     private bool IsGrounded()
     {
         jumpCount = 0;
         
         return Physics.CheckCapsule(cap.bounds.center,
             new Vector3(cap.bounds.center.x, cap.bounds.min.y, cap.bounds.center.z), cap.radius * .9f, groundLayers);
-        
     }
-
     private void ApplyInput(float moveInput, float turnInput)
     {
         Move(moveInput);
         Turn(turnInput);
     }
-
-
     private void Move(float input)
     {
         rb.AddForce(moveSpeed * input * transform.forward, ForceMode.Force);
     }
-
     private void Turn(float input)
     {
         transform.Rotate(0, input * rotationRate * Time.deltaTime, 0);
---
+++ DGM1610 Restart/Assets/Scripts/UITextController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+// SAME DATA TYPE
+//STRING IS FOR TEXT
+
+
+[RequireComponent(typeof(Text))]
+
+public class UITextController : MonoBehaviour
+{
+    public UnityEvent startEvent;
+    private Text textLabel;
+
+    public void UpdateText(IntDataObject data)
+    {
+        textLabel.text = data.value.ToString();
+    }
+
+
+    void Start()
+    {
+        textLabel = GetComponent<Text>();
+        startEvent.Invoke();
+    }
+}
