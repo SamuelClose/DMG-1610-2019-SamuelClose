a0e91c95b076b42c061d4ebf7e7b691f8a1cdf65
Samuel Close <10911389@my.uvu.edu>
2019-10-09 09:11:57 -0600

    SO prefab player spawn/charcter creation

---
+++ DGM1610 Restart/Assets/Scripts/InClassScripts/IntDataObject.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu]
+public class IntDataObject : ScriptableObject
+{
+    public int value = 1;
+
+    public void UpdateValue(int number)
+    {
+        value += number;
+        
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+}
